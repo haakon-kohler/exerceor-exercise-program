12fae75f4f000413fc6e79c47af0cf94ce8e3021
haakon-kohler <126187428+haakon-kohler@users.noreply.github.com>
2023-02-23 13:34:36 -0500

    Program re-add
    Very sorry for completely messing up the repositories! Still getting the hang of this

---
+++ Program.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+public class Program {
+    // The overall exercise program, data structure for individual list
+    private String startString;
+    private String name;
+    private ArrayList cycleList;
+
+    public void createProgram(String startIn, String nameIn) 
+    {
+        startString = startIn;
+        name = nameIn;
+    }
+
+    public void createProgram(String startIn, String nameIn, Cycle<E> firstCycle) 
+    {
+        startString = startIn;
+        name = nameIn;
+        cycleList.add(firstCycle);
+    }
+
+    public getStart()
+    {
+        return (startString);
+    }
+
+    public setStart(String startIn)
+    {
+        startString = startIn;
+    }
+
+    public getName()
+    {
+        return (name);
+    }
+
+    public setName(String nameIn)
+    {
+        name = nameIn;
+    }
+
+    public addCycle(Cycle<E> cycleIn)
+    {
+        cycleList.add(cycleIn);
+    }
+}
