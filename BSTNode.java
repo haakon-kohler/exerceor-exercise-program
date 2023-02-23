bf30d0bce92c76040af90d5b7f0f79a2a1592a1a
nrose2023 <126187727+nrose2023@users.noreply.github.com>
2023-02-23 11:39:58 -0500

    Update BSTNode.java

--- BSTNode.java
+++ BSTNode.java
@@ -1,1 +1,46 @@
-// ! Add nick's BST code
+public class BSTNode<E extends Comparable<E>> {
+	    private E value;    //value must be Comparable
+	    private BSTNode<E> left;
+	    private BSTNode<E> right;
+
+	    //constructor(s)
+	    BSTNode(E val){
+	        left = null;
+	        right = null;
+	        value = val;
+	    }
+
+	    //getters and setters for value left and right
+	    public BSTNode<E> getLeft()
+	    {
+	        return left;
+	    }
+
+	    public BSTNode<E> getRight()
+	    {
+	        return right;
+	    }
+
+	    public void setLeft(BSTNode<E> node)
+	    {
+	        left = node;
+	    }
+
+	    public void setRight(BSTNode<E> node)
+	    {
+	        right = node;
+	    }
+
+	    public E getValue()
+	    {
+	        return value;
+	    }
+
+	    public boolean isLeaf()
+	    {
+	        if(getRight() == null && getLeft() == null){
+	            return true;
+	        }
+	        return false;
+	    }
+	}
