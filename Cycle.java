788cc3dae998cdb9e07a599feda950d29c838015
haakon-kohler <haakon.kohler@gmail.com>
2023-02-23 12:44:10 -0500

    Re-adding cycle.java

---
+++ Cycle.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Cycle<Lift extends BST<E>> {
+    private int startString;
+    private int endString;
+
+    public getStart()
+    {
+        return (startString);
+    }
+
+    public setStart(String startIn)
+    {
+        startString = startIn;
+    }
+
+    public getName()
+    {
+        return (name);
+    }
+
+    public setName(String nameIn)
+    {
+        name = nameIn;
+    }
+}
