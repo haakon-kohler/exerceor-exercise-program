4a30e1cc2995b52675bc47f58c61e2b40a79c110
haakon-kohler <126187428+haakon-kohler@users.noreply.github.com>
2023-02-23 12:27:38 -0500

    Update cycle
    This is an update to cycle to add name and date functions

--- Cycle.java
+++ Cycle.java
@@ -4,4 +4,24 @@ import java.util.Date;
 public class Cycle<Lift extends BST<E>> {
     private int startString;
     private int endString;
+
+    public getStart()
+    {
+        return (startString);
+    }
+
+    public setStart(String startIn)
+    {
+        startString = startIn;
+    }
+
+    public getName()
+    {
+        return (name);
+    }
+
+    public setName(String nameIn)
+    {
+        name = nameIn;
+    }
 }
