a4552ace1e642503da6d65fe2ec3ea02538ffcec
nrose2023 <126187727+nrose2023@users.noreply.github.com>
2023-02-24 10:49:16 -0500

    Update Cycle.java

--- Cycle.java
+++ Cycle.java
@@ -5,23 +5,40 @@ public class Cycle<Lift extends BST<E>> {
     private int startString;
     private int endString;
 
-    public getStart()
+    public String getStart()
     {
         return (startString);
     }
 
-    public setStart(String startIn)
+    public void setStart(String startIn)
     {
         startString = startIn;
     }
 
-    public getName()
+    public String getName()
     {
         return (name);
     }
 
-    public setName(String nameIn)
+    public void setName(String nameIn)
     {
         name = nameIn;
+    }
+    
+    public void PrintCycle(Cycle cycle1) {
+    	try {
+    	File file = new File("/Users/nicholasrose/Desktop/workoutplan.txt");
+		file.createNewFile();
+		
+		FileWriter myWriter = new FileWriter("workoutplan.txt");
+		
+		myWriter.write(cycle1.toString());
+		
+		myWriter.close();
+		
+    	}
+    	catch (Exception IOException) {
+    		
+    	}
     }
 }
